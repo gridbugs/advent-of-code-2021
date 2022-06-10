12486d821dd518b154c13aa4626f0341932f3452
Stephen Sherratt <stephen@sherra.tt>
2022-06-10 19:45:54 +1000

    Flakify

--- shell.nix
+++ shell.nix
@@ -1,31 +1,9 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-  ocamlInit = pkgs.writeText "ocamlinit" ''
-    #require "base";;
-    #require "core";;
-  '';
-in
-pkgs.mkShell {
-  nativeBuildInputs = with pkgs; [
-    dune_2
-  ] ++ (with ocamlPackages;
-  [
-    ocaml
-    ocaml-lsp
-    ocamlformat
-    core
-    core_extended
-    re2
-    findlib
-    utop
-    merlin
-    ppx_sexp_conv
-    ppx_fields_conv
-    ppx_deriving
-    ppx_expect
-    ppx_compare
-  ]);
-  shellHook = ''
-    alias utop="utop -init ${ocamlInit}"
-  '';
-}
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash; }
+) {
+  src =  ./.;
+}).shellNix
